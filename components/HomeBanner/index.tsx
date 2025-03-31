daedac41d73b6d4e4031b5313200b4c45ef44648
claudiojas <claudiojasoares.dev@gmail.com>
2025-03-31 02:12:02 -0300

    fix: adaptando para usar a lib react-slick no componente de bunner

--- components/HomeBanner/index.tsx
+++ components/HomeBanner/index.tsx
@@ -1,9 +1,10 @@
-'use client'; // Diretiva que marca o arquivo como cliente
+'use client';
 
-import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Image, { StaticImageData } from "next/image";
 import { banner1, banner2, banner3 } from "@immu/assets";
-
 
 interface ICarouselItemsProps {
   image: StaticImageData;
@@ -27,44 +28,24 @@ const carouselItems: ICarouselItemsProps[] = [
   },
 ];
 
-
-export default function HomeBanner () {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const totalItems = carouselItems.length;
-
-  // Função para avançar o carrossel
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalItems); // Loop infinito
+export default function HomeBanner() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
   };
-
-  // Função para voltar o carrossel
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + totalItems) % totalItems
-    ); // Loop infinito
-  };
-
-  // Autoplay
-  useEffect(() => {
-    const intervalId = setInterval(nextSlide, 4000); // Passa o slide a cada 3 segundos
-
-    return () => clearInterval(intervalId); // Limpar intervalo ao desmontar
-  }, []);
 
   return (
-    <div className="relative w-full overflow-hidden">
-      {/* Contêiner do carrossel, com a transição */}
-      <div
-        className="flex transition-transform duration-500"
-        style={{
-          transform: `translateX(-${currentIndex * 100}%)`,
-        }}
-      >
+    <div className="relative w-full pb-6 overflow-hidden">
+      <Slider {...settings}>
         {carouselItems.map((item, index) => (
-          <div key={index} className="w-full flex-shrink-0 relative group">
-            {/* Camada de fundo escurecendo no hover */}
+          <div key={index} className="w-full relative group">
             <div className="absolute inset-0 bg-black bg-opacity-5 group-hover:bg-opacity-10 transition-all"></div>
-
+            
             <Image
               src={item.image}
               alt={`Slide ${index + 1}`}
@@ -74,27 +55,18 @@ export default function HomeBanner () {
             />
 
             <span className="absolute top-[15.8rem] right-[5.1rem] flex flex-col gap-4 z-10">
-              <p className="whitespace-pre-line text-white font-bold font-murecho text-6xl text-right">{item.text}</p>
-              <p className="whitespace-pre-line text-white font-bold font-murecho text-lg text-right">{item.subText}</p>
+              <p className="whitespace-pre-line text-white font-bold font-murecho text-6xl text-right">
+                {item.text}
+              </p>
+              {item.subText && (
+                <p className="whitespace-pre-line text-white font-bold font-murecho text-lg text-right">
+                  {item.subText}
+                </p>
+              )}
             </span>
-          </div> 
+          </div>
         ))}
-      </div>
-
-      {/* Botões de navegação */}
-      <button
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-4 rounded-full"
-        onClick={prevSlide}
-      >
-        &#10094;
-      </button>
-      <button
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-4 rounded-full"
-        onClick={nextSlide}
-      >
-        &#10095;
-      </button>
+      </Slider>
     </div>
   );
-};
-
+}
