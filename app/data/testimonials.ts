791862667b343e99778546de3696ff2b38d9258a
claudiojas <claudiojasoares.dev@gmail.com>
2025-04-16 02:35:45 -0300

    feat: conclui o componente de CardTestimonials

--- app/data/testimonials.ts
+++ app/data/testimonials.ts
@@ -1,9 +1,11 @@
-import imageTestimonialExemple from "../../assets/testimonial/Luciene Foto.jpg";
+import LucileneFoto from "../../assets/testimonial/Luciene Foto.jpg";
+import MLurdesFoto from "../../assets/testimonial/Maria de Lurdes Foto.jpg";
+import MarianeFoto from "../../assets/testimonial/Mariane Foto.jpg";
 
 
 export const dataTestimonial = [
     {
-      imageTestimonial: imageTestimonialExemple,
+      imageTestimonial: LucileneFoto,
       testimonial:
         `Tenho mais concentração, ali tem bate papo, tem pintura, 
         aprendemos a pintar pano de prato e a fazer pulseiras. 
@@ -14,24 +16,22 @@ export const dataTestimonial = [
       duration: "2 anos",
     },
     {
-      imageTestimonial: imageTestimonialExemple,
+      imageTestimonial: MLurdesFoto,
       testimonial:
-        `Tenho mais concentração, ali tem bate papo, tem pintura, 
-        aprendemos a pintar pano de prato e a fazer pulseiras. 
-        É muito bom estar no Instituto. Assista o vídeo de Luciene`,
-      infoTestimonial: "Assista o vídeo de Luciene",
-      name: "Luciene",
+        `A gente entra uma pessoa e saí outra, totalmente renovada. 
+        Ali é lugar da gente trabalhar e aprender a fazer as coisas para se manter`,
+      infoTestimonial: "Assista o vídeo de Maria de Lourdes",
+      name: "Maria de Lourdes",
       age: "30",
       duration: "2 anos",
     },
     {
-      imageTestimonial: imageTestimonialExemple,
+      imageTestimonial: MarianeFoto,
       testimonial:
-        `Tenho mais concentração, ali tem bate papo, tem pintura, 
-        aprendemos a pintar pano de prato e a fazer pulseiras. 
-        É muito bom estar no Instituto. Assista o vídeo de Luciene`,
-      infoTestimonial: "Assista o vídeo de Luciene",
-      name: "Luciene",
+        `Meu nome é Mariane , eu faço parte do instituto desde 2022 ,
+        desde então eu me sinto outra pessoa , o instituto nos oferece várias atividades como artesanato, arteterapia etc`,
+      infoTestimonial: "Assista o vídeo de Mariane",
+      name: "Mariane",
       age: "30",
       duration: "2 anos",
     },
--- components/CardTestimonials/index.tsx
+++ components/CardTestimonials/index.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 
 interface ICardTestimoniasPropa {
   testimonial: string;
@@ -36,7 +37,7 @@ export default function CardTestimonials({
         <p className="max-w-full w-[378px] text-lg italic font-medium">
           &quot; &#91; &hellip; &#93; {testimonial}&quot;
         </p>
-        <a className="text-textViolet font-semibold text-lg">{infoTestimonial}</a>
+        <Link className="text-textViolet font-semibold text-lg cursor-pointer hover:opacity-85" href={""}>{infoTestimonial}</Link>
       </span>
     </div>
   );
